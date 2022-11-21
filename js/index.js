54c7cf061e4b63bff8e65cfce91796dc10c4f981
Alex Ramirez <alexir13@gmail.com>
2022-11-21 14:23:50 -0800

    Bar charts

--- js/index.js
+++ js/index.js
@@ -19,13 +19,39 @@ var self = {
             row.remove();
         }
 
+        var maxPercentage = self.stats.reduce((a, b) => a.meatball_percent > b.meatball_percent ? a : b).meatball_percent;
+
         // Populate rows
         for (let player of self.stats){
             var row = document.createElement('tr');
             var td1 = document.createElement('td');
             var td2 = document.createElement('td');
 
+            td1.className = 'player';
             td1.innerHTML = player[' first_name'] + ' ' + player.last_name;
+
+            td2.className = 'chart';
+
+            var meatballPercDiv = document.createElement('div');
+            meatballPercDiv.className = 'meatballperc';
+
+            var widthPerc = (player.meatball_percent / maxPercentage) * 100;
+            var widthPercStr = widthPerc.toFixed(2) + '%';
+            meatballPercDiv.innerHTML = widthPercStr;
+            td2.appendChild(meatballPercDiv);
+            meatballPercDiv.setAttribute('style', 'width: ' + widthPercStr + ';');
+
+
+
+            var meatballSwingPercDiv = document.createElement('div');
+            meatballSwingPercDiv.className = 'meatballswingperc';
+
+            var widthSwingStr = Number(player.meatball_swing_percent).toFixed(2) + '%';
+            meatballSwingPercDiv.innerHTML = widthSwingStr;
+            meatballPercDiv.appendChild(meatballSwingPercDiv);
+            meatballSwingPercDiv.setAttribute('style', 'width: ' + widthSwingStr + ';');
+            
+            
 
             row.appendChild(td1);
             row.appendChild(td2);
